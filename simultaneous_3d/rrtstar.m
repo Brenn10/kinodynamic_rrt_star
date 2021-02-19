d4affb00a85d207924f2857d901a7116b4f75382
Brennan Cain <brenn.cain@gmail.com>
2021-02-19 17:56:20 -0500

    equations are consistent across states 1-3

--- simultaneous_3d/rrtstar.m
+++ simultaneous_3d/rrtstar.m
@@ -363,24 +363,24 @@ classdef rrtstar
             %states, control, and cost depend on t_s which is the solution from above
             state_dims = size(A,1);
 
-            G = int(expm(A*(obj.t-obj.x))*B/R*B'*expm(A'*(obj.t-obj.x)), obj.x, 0, obj.t);
-            x_bar = expm(A*obj.t)*obj.x0+int(expm(A*(obj.t-obj.x))*c, obj.x, 0, obj.t);
+            G = int(expm(A*(obj.t-obj.x))*B/R*B'*expm(A'*(obj.t-obj.x)), obj.x, 0, obj.t)
+            x_bar = expm(A*obj.t)*obj.x0+int(expm(A*(obj.t-obj.x))*c, obj.x, 0, obj.t)
 
-            d = G\(obj.x1-x_bar);
-            tau_star = 1-2*(A*obj.x1+c)'*d-d'*B/R*B'*d;
+            d = G\(obj.x1-x_bar)
+            tau_star = 1-2*(A*obj.x1+c)'*d-d'*B/R*B'*d
 
             solution = expm([A, B/R*B';zeros(state_dims), -A']*(obj.t-obj.t_s))*[obj.x1;subs(d,obj.t,obj.t_s)]+ ...
-                int(expm([A, B/R*B';zeros(state_dims), -A']*(obj.t-obj.x))*[c; zeros(state_dims,1)],obj.x,obj.t_s,obj.t);
+                int(expm([A, B/R*B';zeros(state_dims), -A']*(obj.t-obj.x))*[c; zeros(state_dims,1)],obj.x,obj.t_s,obj.t)
 
-            control = R\B'*solution(state_dims+1:2*state_dims,:);
-            states = solution(1:state_dims);
+            control = R\B'*solution(state_dims+1:2*state_dims,:)
+            states = solution(1:state_dims)
 
             if exist('dist_idxs','var')
-                sq_distance = sum((obj.x1(dist_idxs)-states(dist_idxs)).^2)-obj.min_dist^2;
+                sq_distance = sum((obj.x1(dist_idxs)-states(dist_idxs)).^2)-obj.min_dist^2
             end
 
-            cost = int(1+control'*R*control, obj.t, 0, obj.t_s);
-
+            cost = int(1+control'*R*control, obj.t, 0, obj.t_s)
+            
         end
 
     end
